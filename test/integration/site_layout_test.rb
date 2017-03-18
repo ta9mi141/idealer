ae66c777de7ad94e69e506579daeda458ebae410
it_akumi <it.akumi1011@gmail.com>
2017-03-18 19:25:22 +0900

    Finish user signup according to rails tutorial chapter 7

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,6 +5,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/top'
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", signup_path
+    # assert_select "a[href=?]", login_path
   end
 end
