a56342af36a581caab6f1f75bd1f393ffde76a60
it_akumi <it.akumi1011@gmail.com>
2017-03-19 17:18:33 +0900

    Implement basic login according to rails tutorial chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,4 +9,9 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  # Return true if test user logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
