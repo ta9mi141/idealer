587187b8041fdff97327c1edcfb6eea25df34ae0
Asahi <sunrise_from_eastern@road.ocn.ne.jp>
2017-03-22 21:47:30 +0900

    drawing is available

--- app/assets/javascripts/carddraw.js
+++ app/assets/javascripts/carddraw.js
@@ -1,19 +1,42 @@
-<!--ドローボタンここ-->
+//<!--ドローボタンここ-->
+var i = 0;
 
 function CardDraw0(){
-  $("#openplace0").replaceWith('<div class="card"><div class="mark">(♠)</div><div class="noteleaf">ここidea<p><a data-target="con1" class="modal-open">[カード派生へ]</a></p>1.2.3.4.</div>');
+  $("#openplace0").replaceWith('<div id="ideacard0" class="card"><div class="mark">'+ ideas[i][1]+'</div><div class="noteleaf">'+ideas[i][0]+'</div></div>');
+  return i++;
+  $(".card").draggable();
 };
 function CardDraw1(){
-  $("#openplace1").replaceWith('<div class="card"><div class="mark">(♠)</div><div class="noteleaf">ここidea<p><a data-target="con2" class="modal-open">[カード派生へ]</a></p>1.2.3.4.</div>');
+  $("#openplace1").replaceWith('<div id="ideacard1" class="card"><div class="mark">'+ideas[i][1]+'</div><div class="noteleaf">'+ideas[i][0]+"</div></div>");
+  return i++
 };
 function CardDraw2(){
-  $("#openplace2").replaceWith('<div class="card"><div class="mark">(♠)</div><div class="noteleaf">ここidea<p><a data-target="con3" class="modal-open">[カード派生へ]</a></p>1.2.3.4.</div>');
+  $("#openplace2").replaceWith('<div id="ideacard2" class="card"><div class="mark">'+ideas[i][1]+'</div><div class="noteleaf">'+ideas[i][0]+"</div></div>");
+  return i++
 };
 function CardDraw3(){
-  $("#openplace3").replaceWith('<div class="card"><div class="mark">(♠)</div><div class="noteleaf">ここidea<p><a data-target="con4" class="modal-open">[カード派生へ]</a></p>1.2.3.4.</div>');
+  $("#openplace3").replaceWith('<div id="ideacard3" class="card"><div class="mark">'+ideas[i][1]+'</div><div class="noteleaf">'+ideas[i][0]+"</div></div>");
+  return i++
 };
 function CardDraw4(){
-  $("#openplace4").replaceWith('<div class="card"><div class="mark">(♠)</div><div class="noteleaf">ここidea<p><a data-target="con5" class="modal-open">[カード派生へ]</a></p>1.2.3.4.</div>');
+  $("#openplace4").replaceWith('<div id="ideacard4" class="card"><div class="mark">'+ideas[i][1]+'</div><div class="noteleaf">'+ideas[i][0]+"</div></div>");
+  return i++
 };
+//<!--ドローボタン-->
 
-<!--ドローボタン-->
+//カード破棄
+function discard0(){
+  $("#ideacard0").replaceWith('<div  id=openplace0>zone0</div>')
+}
+function discard1(){
+  $("#ideacard1").replaceWith('<div  id=openplace1>zone0</div>')
+}
+function discard2(){
+  $("#ideacard2").replaceWith('<div  id=openplace2>zone0</div>')
+}
+function discard3(){
+  $("#ideacard3").replaceWith('<div  id=openplace3>zone0</div>')
+}
+function discard4(){
+  $("#ideacard4").replaceWith('<div  id=openplace4>zone0</div>')
+}
