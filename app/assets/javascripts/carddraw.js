b6174c38a4064f29fa08870ebd6b7a41d33d2d59
Asahi <sunrise_from_eastern@road.ocn.ne.jp>
2017-03-22 15:50:12 +0900

    minor adjust on field

---
+++ app/assets/javascripts/carddraw.js
@@ -0,0 +1,19 @@
+<!--ドローボタンここ-->
+
+function CardDraw0(){
+  $("#openplace0").replaceWith('<div class="card"><div class="mark">(♠)</div><div class="noteleaf">ここidea<p><a data-target="con1" class="modal-open">[カード派生へ]</a></p>1.2.3.4.</div>');
+};
+function CardDraw1(){
+  $("#openplace1").replaceWith('<div class="card"><div class="mark">(♠)</div><div class="noteleaf">ここidea<p><a data-target="con2" class="modal-open">[カード派生へ]</a></p>1.2.3.4.</div>');
+};
+function CardDraw2(){
+  $("#openplace2").replaceWith('<div class="card"><div class="mark">(♠)</div><div class="noteleaf">ここidea<p><a data-target="con3" class="modal-open">[カード派生へ]</a></p>1.2.3.4.</div>');
+};
+function CardDraw3(){
+  $("#openplace3").replaceWith('<div class="card"><div class="mark">(♠)</div><div class="noteleaf">ここidea<p><a data-target="con4" class="modal-open">[カード派生へ]</a></p>1.2.3.4.</div>');
+};
+function CardDraw4(){
+  $("#openplace4").replaceWith('<div class="card"><div class="mark">(♠)</div><div class="noteleaf">ここidea<p><a data-target="con5" class="modal-open">[カード派生へ]</a></p>1.2.3.4.</div>');
+};
+
+<!--ドローボタン-->
